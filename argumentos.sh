afc3bbbdd074232961ab65ab56b4fb0f1cfa4c6b
Felipe Maffezzolli <felipe.maffezzolli@gmail.com>
2019-03-06 11:46:10 -0300

    Dealing with args

---
+++ argumentos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# Capturando argumentos
+#
+
+# Número de argumentos passados
+
+echo "Foram passados $# argumentos para esse script"
+
+# Lista de todos os argumentos
+
+echo "Lista de argumentos $@"
+
+# Printando argumento pelo index
+
+echo "Printando primeiro argumento: $1"
+
